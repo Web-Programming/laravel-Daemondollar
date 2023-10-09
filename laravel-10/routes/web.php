8e93b0604d0e3f3c38fe29da9a718314ffab98fa
Daemond <kingsalmon4659@gmail.com>
2023-10-09 13:08:37 +0700

    making some route and stuff

--- laravel-10/routes/web.php
+++ laravel-10/routes/web.php
@@ -16,3 +16,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+//toute with param (wajib)
+Route::get('/mahasiswa/{nama}', function ($nama = "Michael") {
+    echo "<h1>Nama Saya $nama</h1>";
+});
+
+//toute with param (tidak wajib)
+Route::get('/mahasiswa/{nama?}', function ($nama = "Michael") {
+    echo "<h1>Nama Saya $nama</h1>";
+});
+
+//toute with param > 1
+Route::get('/mahasiswa/{nama?}/{pekerjaan?}', function ($nama = "Michael", $pekerjaan = "Mahasiswa") {
+    echo "<h1>Nama Saya $nama. Saya adalah seorang $pekerjaan</h1>";
+});
+
+//redirect
+Route::get('/hubungi', function () {
+    echo "<h1>Hubungi Kami</h1>";
+})->name("call");
+
+Route::Redirect("/contact", "/hubungi");
+
+Route::get('/profile', function () {
+   echo "<a href='". route('call') . "'>" . route('call'). "</a>";
+});
+
+//route group
+//memudahkan pengelkompokan route per modul
+Route::prefix("/dosen")->group(function(){
+    Route::get("/jadwal", function() {
+        echo "<h1>Jadwal Dosen</h1>";
+    });
+    Route::get("/materi", function() {
+        echo "<h1>Materi Perkuliahan</h1>";
+    });
+    //dan lain2
+});
